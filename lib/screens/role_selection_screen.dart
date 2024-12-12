1ad0ad6a36c563a29ed535b35ade2826d632333a
hasee0001 <kch14570@gmail.com>
2024-12-13 01:16:48 +0545

    role_selectionScreen created

--- lib/screens/role_selection_screen.dart
+++ lib/screens/role_selection_screen.dart
@@ -1,70 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:tottouchordertastemobileapplication/screens/signin_screen.dart';
+import 'package:tottouchordertastemobileapplication/screens/dashboard_screen.dart'; // Assuming you have a DashboardScreen
 
-class RoleSelectionScreen extends StatelessWidget {
+// Convert the RoleSelectionScreen to StatefulWidget to manage hover state and interaction
+class RoleSelectionScreen extends StatefulWidget {
   const RoleSelectionScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _RoleSelectionScreenState createState() => _RoleSelectionScreenState();
+}
+
+class _RoleSelectionScreenState extends State<RoleSelectionScreen> {
+  bool _isHovered = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Select Your Role'),
-        backgroundColor: Colors.deepOrange,
+        title: Text(
+          'Select Your Role',
+          style: GoogleFonts.poppins(
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent,
+        elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30),
-            const Text(
-              "Choose your role to continue",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-                color: Colors.black87,
-                fontFamily: 'Poppins',
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                "Choose your role to start your tasty journey!",
+                style: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orangeAccent,
+                ),
+                textAlign: TextAlign.center,
+              ).animate().fadeIn(duration: 800.ms).slideY(begin: -0.2),
+              const SizedBox(height: 30),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  itemCount: roles.length,
+                  itemBuilder: (context, index) {
+                    final role = roles[index];
+                    return _buildRoleCard(
+                      context,
+                      title: role['title'] as String,
+                      icon: role['icon'] as IconData,
+                      backgroundColor: role['color'] as Color,
+                      onTap: role['action'] != null
+                          ? () => (role['action'] as Function)(context)
+                          : () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => role['route'] as Widget,
+                                ),
+                              );
+                            },
+                    ).animate().fadeIn(duration: (800 + index * 100).ms)
+                      ..slideY(begin: -0.2);
+                  },
+                ),
               ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 15,
-                mainAxisSpacing: 15,
-                children: [
-                  _buildRoleCard(
-                    context,
-                    title: 'User',
-                    icon: Icons.person_outline,
-                    backgroundColor: Colors.grey.shade200,
-                    onTap: () {
-                      Navigator.pushNamed(context, '/user-dashboard');
-                    },
-                  ),
-                  _buildRoleCard(
-                    context,
-                    title: 'Restaurant',
-                    icon: Icons.restaurant_outlined,
-                    backgroundColor: Colors.orange.shade100,
-                    onTap: () {
-                      Navigator.pushNamed(context, '/restaurant-dashboard');
-                    },
-                  ),
-                  _buildRoleCard(
-                    context,
-                    title: 'Guest',
-                    icon: Icons.table_chart_outlined,
-                    backgroundColor: Colors.deepOrange.shade100,
-                    onTap: () {
-                      _showGuestLoginPopup(context);
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -77,45 +87,53 @@ class RoleSelectionScreen extends StatelessWidget {
     required Color backgroundColor,
     required VoidCallback onTap,
   }) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.circular(8), // Rounded rectangle border
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 8,
-              offset: Offset(0, 4),
-            ),
-          ],
-        ),
-        child: Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
+    return MouseRegion(
+      onEnter: (_) => _hoverAnimation(true),
+      onExit: (_) => _hoverAnimation(false),
+      child: GestureDetector(
+        onTap: onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: _isHovered
+                ? [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                      offset: const Offset(0, 6),
+                    ),
+                  ]
+                : [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      spreadRadius: 3,
+                      blurRadius: 6,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
           ),
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+            padding: const EdgeInsets.all(20),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
                   icon,
-                  size: 50,
-                  color: Colors.deepOrange,
-                ),
-                const SizedBox(height: 10),
+                  size: _isHovered ? 70 : 60,
+                  color: Colors.deepOrangeAccent,
+                ).animate().scale(duration: 600.ms),
+                const SizedBox(height: 12),
                 Text(
                   title,
-                  style: const TextStyle(
+                  style: GoogleFonts.poppins(
                     fontSize: 18,
                     fontWeight: FontWeight.w600,
                     color: Colors.black87,
-                    fontFamily: 'Poppins',
                   ),
-                  textAlign: TextAlign.center,
                 ),
               ],
             ),
@@ -125,43 +143,127 @@ class RoleSelectionScreen extends StatelessWidget {
     );
   }
 
-  void _showGuestLoginPopup(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Guest Login'),
-          content: const Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Enter your table number to proceed:'),
-              SizedBox(height: 10),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Table Number',
-                  border: OutlineInputBorder(),
+  void _hoverAnimation(bool isHovered) {
+    setState(() {
+      _isHovered = isHovered;
+    });
+  }
+}
+
+final roles = [
+  {
+    'title': 'User',
+    'icon': Icons.person_outline,
+    'color': Colors.orange.shade200,
+    'route': const SignInScreen(),
+  },
+  {
+    'title': 'Restaurant',
+    'icon': Icons.restaurant,
+    'color': Colors.red.shade200,
+    'route': const SignInScreen(),
+  },
+  {
+    'title': 'Guest',
+    'icon': Icons.fastfood_outlined,
+    'color': Colors.green.shade200,
+    'action': (BuildContext context) {
+      _showGuestLoginPopup(context);
+    },
+  },
+];
+
+void _showGuestLoginPopup(BuildContext context) {
+  final TextEditingController tableNumberController = TextEditingController();
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        title: Text(
+          'Guest Login',
+          style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Enter your table number to proceed:',
+              style: GoogleFonts.poppins(fontSize: 16),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: tableNumberController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Table Number',
+                hintText: 'e.g., 101',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                    color: Colors.orangeAccent,
+                    width: 1.5,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                    color: Colors.orangeAccent,
+                    width: 2.0,
+                  ),
                 ),
               ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/guest-dashboard');
-              },
-              child: const Text('Proceed'),
             ),
           ],
-        );
-      },
-    );
-  }
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Cancel',
+              style: GoogleFonts.poppins(fontSize: 16),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepOrangeAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            onPressed: () {
+              if (tableNumberController.text.isNotEmpty) {
+                Navigator.pop(context); // Close the dialog
+                // Navigate to DashboardScreen after closing dialog
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const DashboardScreen(),
+                  ),
+                );
+              } else {
+                // Show an error or validation message if no table number is entered
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Please enter a valid table number.',
+                      style: GoogleFonts.poppins(),
+                    ),
+                  ),
+                );
+              }
+            },
+            child: Text(
+              'Proceed',
+              style: GoogleFonts.poppins(fontSize: 16),
+            ),
+          ),
+        ],
+      );
+    },
+  );
 }
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,7 +1,5 @@
-// lib/screens/signin_screen.dart
 import 'package:flutter/material.dart';
 import 'package:tottouchordertastemobileapplication/screens/login_screen.dart';
-
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
@@ -9,38 +7,200 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sign In')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const TextField(
-              decoration: InputDecoration(labelText: 'Email'),
+      appBar: AppBar(
+        backgroundColor:
+            Colors.white, // Solid background color for a clean look
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.deepOrange),
+          onPressed: () {
+            Navigator.pop(context); // Go back to previous screen
+          },
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Image.asset(
+              'assets/images/AppLogo.png',
+              height: 30,
+            ), // Small logo in the app bar
+          ),
+        ],
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // App Logo
+                CircleAvatar(
+                  radius: 80,
+                  backgroundColor: Colors.orange.shade100,
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/images/AppLogo.png',
+                      fit: BoxFit.cover,
+                      height: 160,
+                      width: 160,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+
+                // Name Input Fields
+                const Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'First Name',
+                          labelStyle: TextStyle(color: Colors.black87),
+                          filled: true,
+                          fillColor: Color.fromARGB(255, 166, 96, 52),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 18, horizontal: 15),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Last Name',
+                          labelStyle: TextStyle(color: Colors.black87),
+                          filled: true,
+                          fillColor: Color.fromARGB(255, 166, 96, 52),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 18, horizontal: 15),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+
+                // Email Input Field
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.black87),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 166, 96, 52),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 18, horizontal: 15),
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                // Phone Number Input Field
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    labelStyle: TextStyle(color: Colors.black87),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 166, 96, 52),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 18, horizontal: 15),
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                // Password Input Field
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.black87),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 166, 96, 52),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 18, horizontal: 15),
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                // Confirm Password Input Field
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Confirm Password',
+                    labelStyle: TextStyle(color: Colors.black87),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 166, 96, 52),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 18, horizontal: 15),
+                  ),
+                ),
+                const SizedBox(height: 25),
+
+                // Sign In Button
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 15),
+                    backgroundColor: const Color.fromARGB(255, 241, 130, 96),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    elevation: 5,
+                  ),
+                  onPressed: () {
+                    // Logic to handle sign-in
+                  },
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+                const SizedBox(height: 25),
+
+                // Login Text Button
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()),
+                    );
+                  },
+                  child: const Text(
+                    'Already have an account? Log In',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 100, 30, 8), fontSize: 16),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 10),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Logic to handle sign-in
-              },
-              child: const Text('Sign In'),
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginScreen()),
-                );
-              },
-              child: const Text('Already have an account? Log In'),
-            ),
-          ],
+          ),
         ),
       ),
     );
